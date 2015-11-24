a308c794eda4755171f5353bf9d726fc915b2064
Brandon Silva <itbrandonsilva@gmail.com>
2015-11-24 04:28:36 -0500

    Refactored.

--- src/hooker.js
+++ src/hooker.js
@@ -4,69 +4,75 @@ var spawnSync = require('child_process').spawnSync;
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var argv = require('yargs').argv;
 
-var Hooker = function (options) {
-    var self = this;
+class Hooker {
+    constructor(options) {
+        var appNames = Object.keys(options.config);
+        var apps = appNames.map(appName => {
+            var app = options.config[appName];
+            app.name = appName;
+            return app;
+        });
+        this.port = options.port || 17892;
 
-    this.repoPath = options.repository || process.cwd();
-    this.port = options.port || 17892;
-    this.hookPath = options.hookPath || function () {
-        var split = self.repoPath.split('/');
-        return "/" + split[split.length-1]; 
-    }();
-    this.bash = options.bash;
+        this.paths = new Map();
+        Object.keys(options.config).forEach(appName => {
+            var app = options.config[appName];
+            var path = appName || app.path.split('/').pop();
+            this.paths.set('/' + path, app);
+        });
 
-    if (options.daemon) {
-        console.log("Daemonizing...");
-        require('daemon')();
-        this.logfd = fs.openSync(path.join(process.cwd(), (typeof options.log == 'string' ? options.log : 'hooker.log')), 'a');
-        this.log("\n\n---------------------------------");
-        this.log("Started: " + new Date());
-        this.log("Daemonized PID: " + process.pid);
+        if (options.daemon) {
+            console.log("Daemonizing...");
+            require('daemon')();
+            this.logfd = fs.openSync(path.join(process.cwd(), (typeof options.log == 'string' ? options.log : 'hooker.log')), 'a');
+            this.log("\n\n---------------------------------");
+            this.log("Started: " + new Date());
+            this.log("Daemonized PID: " + process.pid);
+        }
+
+        var httpServer = new http.createServer((req, res) => {
+            if (req.url == "/favicon.ico") return res.end();
+            this.log("\n---------------------");
+            this.log(new Date());
+            this.log("Path: " + req.url);
+            var app = this.paths.get(req.url);
+            if (app) {
+                this.pull(app);
+                res.write("ok");
+                res.end();
+            }
+        });
+
+        httpServer.listen(this.port, () => {
+            this.log("Configured for: " + appNames.join(', '));
+            this.log("Listening on port: " + this.port);
+        });
     }
-    
-    var httpServer = new http.createServer(function (req, res) {
-        if (req.url == "/favicon.ico") return res.end();
-        self.log("\n---------------------");
-        self.log(new Date());
-        self.log("Path: " + req.url);
-        if (req.url != self.hookPath) return res.end();
-        self.pull();
-        res.write("ok");
-        res.end();
-    });
-    httpServer.listen(this.port, null, null, function () {
-        self.log("Configured for: " + self.repoPath);
-        self.log("Listening on port: " + self.port);
-        self.log("Hook path set to: " + self.hookPath);
-    });
+
+    pull(app) {
+        var config = {cwd: app.path, stdio: [this.logfd || 0, this.logfd || 1, this.logfd || 2]};
+        this.log("EXEC: Pull (" + app.path + ")");
+        spawnSync("git", ["pull"], config);
+        this.log("EXEC: Bash");
+        if (app.bash) spawnSync("/bin/bash", app.bash, config);
+    }
+
+    log(data) {
+        if (!this.logfd) return console.log(data);
+        fs.writeSync(this.logfd, data + "\n");
+    }
 };
 
-Hooker.prototype.pull = function () {
-    var config = {cwd: this.repoPath, stdio: [this.logfd || 0, this.logfd || 1, this.logfd || 2]};
-    this.log("EXEC: Pull");
-    spawnSync("git", ["pull"], config);
-    this.log("EXEC: Bash");
-    if (this.bash) spawnSync("/bin/bash", [path.join(process.cwd(), this.bash)], config);
-};
-
-Hooker.prototype.log = function (data) {
-    if (!this.logfd) return console.log(data);
-    fs.writeSync(this.logfd, data + "\n");
-};
 
 module.exports = Hooker;
 
 if (require.main === module) {
-    var argv = require('yargs').argv;
-    var repoPath = (argv.repo ? process.cwd() + "/" + argv.repo : null);
-
     new Hooker({
-        repository: repoPath,
+        config: require(path.join(process.cwd(), 'hooker.json')),
         port: argv.port,
-        hookPath: argv.path,
-        log: argv.log,
         daemon: argv.daemon,
-        bash: argv.bash
+        //log: argv.log,
     });
 }
